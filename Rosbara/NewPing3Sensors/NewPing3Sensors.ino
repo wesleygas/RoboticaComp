6fd06d0f2600e97b0b251d17079e68edb4ea0fe0
wesleygas <wesleygl16@gmail.com>
2018-06-08 18:04:32 -0300

    Ping with mouse

---
+++ Rosbara/NewPing3Sensors/NewPing3Sensors.ino
@@ -0,0 +1,213 @@
+// ---------------------------------------------------------------------------
+// Example NewPing library sketch that pings 3 sensors 20 times a second.
+// ---------------------------------------------------------------------------
+
+#include <NewPing.h>
+
+#define SONAR_NUM 3     // Number of sensors.
+#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
+
+#define MDATA 5
+#define MCLK 6
+
+unsigned long currentMillis = 0;
+unsigned long sonarMillis = 0;
+
+String toOut = "";
+int dist = 0;
+
+NewPing sonar[SONAR_NUM] = {   // Sensor object array.
+  NewPing(3, 2, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
+  NewPing(11, 4, MAX_DISTANCE), 
+  NewPing(13, 12, MAX_DISTANCE)
+};
+
+float readUltrass(){
+  toOut = "$:";
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.   
+    dist = 0;
+    dist = sonar[i].ping_cm();
+    delay(40);
+    toOut += String(dist);
+    toOut += ',';
+  }
+  toOut += 'w';
+  Serial.println(toOut);
+  
+}
+
+void gohi(int pin)
+{
+  pinMode(pin, INPUT);
+  digitalWrite(pin, HIGH);
+}
+
+void golo(int pin)
+{
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+}
+
+void mouse_write(char data)
+{
+  char i;
+  char parity = 1;
+
+  //  Serial.print("Sending ");
+  //  Serial.print(data, HEX);
+  //  Serial.print(" to mouse\n");
+  //  Serial.print("RTS");
+  /* put pins in output mode */
+  gohi(MDATA);
+  gohi(MCLK);
+  delayMicroseconds(300);
+  golo(MCLK);
+  delayMicroseconds(300);
+  golo(MDATA);
+  delayMicroseconds(10);
+  /* start bit */
+  gohi(MCLK);
+  /* wait for mouse to take control of clock); */
+  while (digitalRead(MCLK) == HIGH)
+    ;
+  /* clock is low, and we are clear to send data */
+  for (i=0; i < 8; i++) {
+    if (data & 0x01) {
+      gohi(MDATA);
+    } 
+    else {
+      golo(MDATA);
+    }
+    /* wait for clock cycle */
+    while (digitalRead(MCLK) == LOW)
+      ;
+    while (digitalRead(MCLK) == HIGH)
+      ;
+    parity = parity ^ (data & 0x01);
+    data = data >> 1;
+  }  
+  /* parity */
+  if (parity) {
+    gohi(MDATA);
+  } 
+  else {
+    golo(MDATA);
+  }
+  while (digitalRead(MCLK) == LOW)
+    ;
+  while (digitalRead(MCLK) == HIGH)
+    ;
+  /* stop bit */
+  gohi(MDATA);
+  delayMicroseconds(50);
+  while (digitalRead(MCLK) == HIGH)
+    ;
+  /* wait for mouse to switch modes */
+  while ((digitalRead(MCLK) == LOW) || (digitalRead(MDATA) == LOW))
+    ;
+  /* put a hold on the incoming data. */
+  golo(MCLK);
+  //  Serial.print("done.\n");
+}
+
+/*
+ * Get a byte of data from the mouse
+ */
+char mouse_read(void)
+{
+  char data = 0x00;
+  int i;
+  char bit = 0x01;
+
+  //  Serial.print("reading byte from mouse\n");
+  /* start the clock */
+  gohi(MCLK);
+  gohi(MDATA);
+  delayMicroseconds(50);
+  while (digitalRead(MCLK) == HIGH)
+    ;
+  delayMicroseconds(5);  /* not sure why */
+  while (digitalRead(MCLK) == LOW) /* eat start bit */
+    ;
+  for (i=0; i < 8; i++) {
+    while (digitalRead(MCLK) == HIGH)
+      ;
+    if (digitalRead(MDATA) == HIGH) {
+      data = data | bit;
+    }
+    while (digitalRead(MCLK) == LOW)
+      ;
+    bit = bit << 1;
+  }
+  /* eat parity bit, which we ignore */
+  while (digitalRead(MCLK) == HIGH)
+    ;
+  while (digitalRead(MCLK) == LOW)
+    ;
+  /* eat stop bit */
+  while (digitalRead(MCLK) == HIGH)
+    ;
+  while (digitalRead(MCLK) == LOW)
+    ;
+
+  /* put a hold on the incoming data. */
+  golo(MCLK);
+  //  Serial.print("Recvd data ");
+  //  Serial.print(data, HEX);
+  //  Serial.print(" from mouse\n");
+  return data;
+}
+
+void mouse_init()
+{
+  gohi(MCLK);
+  gohi(MDATA);
+  //  Serial.print("Sending reset to mouse\n");
+  mouse_write(0xff);
+  mouse_read();  /* ack byte */
+  //  Serial.print("Read ack byte1\n");
+  mouse_read();  /* blank */
+  mouse_read();  /* blank */
+  //  Serial.print("Sending remote mode code\n");
+  mouse_write(0xf0);  /* remote mode */
+  mouse_read();  /* ack */
+  //  Serial.print("Read ack byte2\n");
+  delayMicroseconds(100);
+}
+
+
+void setup() {
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  mouse_init();
+  
+}
+
+void loop() { 
+  currentMillis = millis();
+    if(currentMillis - sonarMillis > 300){
+      sonarMillis = currentMillis;
+      readUltrass();
+    }
+
+  char mstat;
+  int mx;
+  int my;
+
+  /* get a reading from the mouse */
+  mouse_write(0xeb);  /* give me data! */
+  mouse_read();      /* ignore ack */
+  mstat = mouse_read();
+  mx = mouse_read();
+  my = - mouse_read();
+
+  /* send the data back up */
+  Serial.print("%:");
+  Serial.print(mx);
+  Serial.print(",");
+  Serial.print(my);
+  Serial.println(",w");
+  delay(10);  /* twiddle */
+ 
+}
+
+
